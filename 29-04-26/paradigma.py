50d5fa6e1a83cd40fc049df363a0939cd0000f94
LauraGzJones <lauragzjones@gmail.com>
2026-04-29 21:39:51 -0400

    hola

---
+++ 29-04-26/paradigma.py
@@ -0,0 +1,50 @@
+''' PARADIGMA 
+Un paradigma es una forma de pensar y organizar,en este caso el codigo 
+PARADIGMA ORIENTADO A OBJETOS atributos, acciones, caracteristicas,
+
+TAREA : IDENTIFICAR OBJ
+ETOS TANJIBLES(automoviles) Y INTANGIBLES () EN LA VIDA COTIDIANA.
+
+CLASE = Es una plantilla, molde, o  matriz que se utiliza para crear objetos, la clase define los atributos y 
+metodos que tendran los objetos creados a partir de ella.La clase, me sirve para crear muchos objetos, cada objeto tiene sus propios 
+atributos, pero todos los objetos de la misma clase tienen los mismos atributos.
+
+
+ATRIBUTOS  = Caracteristicas de un objeto, altura peso, color, marca, modelo, año, rut 
+no todos los atributos  son iguales 
+
+GET es para obtener el valor de un atributo. 
+no cambia el valor del atributo, solo lo muestra.
+
+
+SET es para asignar un valor a un atributo. Se usa para modificar el valor de un atributo. 
+cambia el valor del atributo 
+
+
+
+
+
+METODOS  O ACCIONES = Lo que el objeto puede hacer, por ejemplo, un automovil puede acelerar,
+ frenar, girar,cambiar de marcha, cargar combustible, etc.
+una persona puede caminar, hablar, comer, etc.
+ej =  clase PERSONA quiere bajar de peso 
+Metodo = hacer ejercicio, comer sano, dormir bien, etc.
+clase automovil 
+ metodos = acelerar, frenar, girar, cambiar de marcha, cargar combustible, etc.
+no todos los atributos se pueden modificar, por ejemplo, el numero de serie de un automovil
+ no se puede modificar, pero el color si se puede modificar, mi fecha de nacimineto tampoco se puede 
+ modificar, pero mi peso si se puede modificar.
+ 
+
+HERENCIA = es cuando una clase hereda atributos y metodos de otra clase, por ejemplo, una clase "padre" 
+puede heredar atributos y metodos a una clase "hija", por ejemplo, una clase "Animal" puede ser la clase padre de una clase
+ "Perro", la clase "Perro" heredaria los atributos y metodos de la clase "Animal".
+
+
+
+
+
+
+
+
+'''
